844e06d393e5cce58c8448ad33389f58d963352a
bottodani <daniel88botto@gmail.com>
2020-10-19 19:44:36 +0200

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,18 @@
 import random
 
 def main():
-  dice_rolls = 2
+  dice_rolls = int(input("How many dice would you like to roll?"))
+  dice_size = int(input("Hány oldalú legyen a kocka?"))
   dice_sum = 0
   for i in range(0,dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     dice_sum += roll
-    print(f'You rolled a {roll}')
+    if roll == 1:
+      print(f'You rolled a {roll}! Ez nagy baj!')
+    elif roll == dice_size:
+      print(f"You rolled a {roll}! Gratulálunk!")
+    else:
+      print(f'You rolled a {roll}')
   print(f"Összesen: {dice_sum}")
 
 if __name__== "__main__":
